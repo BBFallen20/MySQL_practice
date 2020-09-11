9f59d4c949c9765f9c264e9a46e90b73c389e751
BBFallen <Danila.333>
2020-09-11 21:45:49 +0300

    Get user function added.

--- control/control_db_class.py
+++ control/control_db_class.py
@@ -7,14 +7,14 @@ class Controller:
         self.database = database
 
     def new_user(self):
-        user_name = 'Anton'
-        user_surname = 'Pavlov'
-        user_patronymic = 'Yurievich'
-        user_date_of_birth = '1998-10-20'
-        user_adress = 'Soborniy, 4'
-        user_salary = 12000
-        user_position = 'Manager'
-        user_department = "Marketing"
+        user_name = input("[?] Enter user name:\n")
+        user_surname = input("[?] Enter user surname:\n")
+        user_patronymic = input("[?] Enter user patronymic:\n")
+        user_date_of_birth = input("[?] Enter user date of birth:\n")
+        user_address = input("[?] Enter user address:\n")
+        user_salary = int(input("[?] Enter user salary:\n"))
+        user_position = input("[?] Enter user position:\n")
+        user_department = input("[?] Enter user department:\n")
         sql = 'INSERT INTO users(name, surname, patronymic, birth_date, adress, salary, position, department) VALUES ' \
               '("%s","%s","%s",STR_TO_DATE(REPLACE("%s", "\'", ""),"%Y-%m-%d"),"%s","%s","%s","%s")'
         val = (
@@ -22,7 +22,7 @@ class Controller:
                   user_surname,
                   user_patronymic,
                   user_date_of_birth,
-                  user_adress,
+                  user_address,
                   user_salary,
                   user_position,
                   user_department
@@ -32,15 +32,48 @@ class Controller:
         return print('[OK] New user has been added.')
 
     def delete_user(self):
-        user_name = 'Anton'
-        user_surname = 'Pavlov'
-        user_adress = 'Soborniy, 4'
+        user_name = input("[?] Enter username:\n")
+        user_surname = input("[?] Enter surname:\n")
+        user_address = input("[?] Enter address:\n")
         sql = 'DELETE FROM users WHERE name = "%s" AND  surname = "%s" AND adress = "%s"'
-        val = (user_name, user_surname, user_adress)
+        val = (user_name, user_surname, user_address)
         self.control.execute(sql, val)
         self.database.commit()
         return print('[OK] User has been deleted.')
 
+    def get_user(self):
+        sql = "SHOW COLUMNS FROM users"
+        self.control.execute(sql)
+        columns = []
+        count = 0
+        for res in self.control.fetchall():
+            columns.append(
+                {
+                    count: res[0]
+                }
+            )
+            count += 1
+        print("[!] Allowed columns:")
+        for i in columns:
+            print(i)
+        search_column = int(input(f"[?] Enter column to search user: \n"))
+        try:
+            val = (input("Enter value:\n") if columns[search_column][search_column] != 'salary' else
+                   int(input("Enter value:\n")), )
+            sql = 'SELECT * FROM users WHERE {column_name} = "%s"'.format(
+                column_name=columns[search_column][search_column]
+            )
+            self.control.execute(sql, val)
+            answer = self.control.fetchall()
+            if answer:
+                for i in answer:
+                    print(i)
+            else:
+                print("[X] No such user.")
+            return print('[OK] Request successful.')
+        except IndexError:
+            return print('[X]! No such column.')
+
 
 a = Controller()
-a.delete_user()
+a.get_user()
